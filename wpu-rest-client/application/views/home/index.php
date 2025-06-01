3d243a6733cc99b79a6028ee70ac7bf458aa7d17
suryarahmadhani24 <suryarahmadhanibb@gmailcom>
2025-06-01 21:41:30 +0700

    pembaharuan video 9

--- wpu-rest-client/application/views/home/index.php
+++ wpu-rest-client/application/views/home/index.php
@@ -1,4 +1,134 @@
+<div class="jumbotron" id="home">
+      <div class="container">
+        <div class="text-center">
+          <img src="<?= base_url(); ?>assets/img/foto.jpg" class="rounded-circle img-thumbnail" width="200">
+          <h1 class="display-4">Surya Rahmadhani Batubara</h1>
+          <h3 class="lead">Student | UI/UX Designer | Dancer</h3>
+          
+        </div>
+      </div>
+    </div>
 
-    <h1>Hello, <?= $nama; ?>!</h1>
+    <!-- Portfolio -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/th.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Lahir di Hutabaringin TB,24 Oktober 2004</p>
+              </div>
+            </div>
+          </div>
 
- 
+          <div class="col-md mb-4">
+            <div class="card">
+           <img class="card-img-top" src="<?= base_url(); ?>assets/img/1.jpg" alt="Card image cap" style="max-height: 200px; object-fit: contain;">
+              <div class="card-body">
+                <p class="card-text">Asal saya dari Mandailing Natal, Sumatera Utara</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/image sekolah.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Sekolah Menengah Atas di MAN 1 Mandailing Natak</p>
+              </div>
+            </div>
+          </div>   
+        </div>
+
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/uin.jpeg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Kuliah di Universitas Islam Negeri Imam Bonjol Padang pada angkatan 2022</p>
+              </div>
+            </div>
+          </div> 
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/sistem informasi.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Jurusan Sistem Informasi, Fakultas Sains dan Teknologi dengan Program Studi Sistem Informasi.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/tari.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Saya sangat suka menari</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Contact -->
+    <section class="contact bg-light" id="contact">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Contact</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="card bg-primary text-white mb-4 text-center">
+              <div class="card-body">
+                <h5 class="card-title">Contact Me</h5>
+                <p class="card-text">Ada ide, pertanyaan, atau kerja sama yang ingin didiskusikan? Mari terhubung dan ngobrol lebih lanjut!.</p>
+              </div>
+            </div>
+            
+            <ul class="list-group mb-4">
+              <li class="list-group-item"><h3>Location</h3></li>
+              <li class="list-group-item">My Home</li>
+              <li class="list-group-item">Mandailing Natal, Sumatera Utara</li>
+              <li class="list-group-item">Indonesia</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-6">
+            
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email">
+              </div>
+              <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" class="form-control" id="phone">
+              </div>
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary">Send Message</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </section>
--- wpu-rest-client/application/views/templates/header.php
+++ wpu-rest-client/application/views/templates/header.php
@@ -21,7 +21,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#">CI Aplikasi</a>
+      <a class="navbar-brand" href="#">CI App</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -31,8 +31,8 @@
           <a class="nav-item nav-link" href="<?= base_url(); ?>">Home
             <span class="sr-only">(current)</span></a>
           <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
-          <a class="nav-item nav-link" href="#">About</a>
-        </div>
+          <a class="nav-item nav-link" href="<?= base_url(); ?>about">About</>
+
       </div>
     </div>
   </nav>
