04595d2af64a884209b0591de3c00be5bb0d9ee6
suryarahmadhani24 <suryarahmadhanibb@gmailcom>
2025-06-01 22:13:36 +0700

    perubahan video 1

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,29 +1,12 @@
-<?php
-
-// // Memberitahu browser bahwa output adalah JSON
- header('Content-Type: application/json');
-
-// // Array data mahasiswa
-// $mahasiswa = [
-//     [
-//         "nama" => "Surya Rahmadhani Batubara",
-//         "nim" => "2217020019",
-//         "email" => "suryarahmadhanibb@gmail.com"
-//     ],
-//     [
-//         "nama" => "Surya",
-//         "nim" => "2217020019",
-//         "email" => "suryabb@gmail.com"
-//     ]
-// ];
-
-$dbh = new PDO('mysql:host=localhost;dbname=mahasiswa', 'root', 
-'root' );
-$db = $dbh->prepare('SELECT * FROM tbl_mahasiswa');
-$db->execute();
-$tbl_mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
-// Encode data ke JSON 
-$data = json_encode ($tbl_mahasiswa);
-echo $data
-
-?>
+ <section class="portfolio" id="portfolio">
+          <div class="row">
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/1.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5>JSON</h5>
+                  <p class="card-text">Latihan API menggunakan file JSON.</p>
+                  <a href="<?= base_url(); ?>../json/latihan1.php" class="card-link see-detail"data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`" >See Project</a>
+                </div>
+              </div>
+            </div>
